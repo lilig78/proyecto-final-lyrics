996ca337c63661c6b1a0fd41741869d5fb9700f1
lilygrande <lili.deleong@gmail.com>
2020-06-18 00:49:32 -0600

    letra , sale error por el limite de consumo de api

--- app-lyrics/src/App.js
+++ app-lyrics/src/App.js
@@ -5,10 +5,13 @@ import Card from './components/Card/Card';
 //HTTP Request => que devuelve una promesa
 import axios from 'axios';
 import './App.css';
+
 function App() {
   //VARIABLES
   const [textoCancion, setTextoCancion] = useState();
   const [nomCancion, setNombreCancion] = useState([]);
+  var letra1 = "";
+
   const requerimiento ='https://cors-anywhere.herokuapp.com/';
   const endpoint ='https://api.musixmatch.com/ws/1.1/';
   const apikey ='fa9bf33f5d2010c8d6909b728684095b';
@@ -26,8 +29,28 @@ function App() {
     .then((info) => setNombreCancion(info.data.message.body.track_list)  ) /* console.log(info.track_list) setNombreCancion(info.message.body) */ /* .then((info) => console.log(info)  ) para traer la data */
     .catch(() => alert("ocurrió un error")) /* () => alert("ocurrió un error") */
   }, [textoCancion]); /* , si está vacío se va a ejecutar cuando el componente se ponga */
-  
 
+
+
+  axios.get(`${requerimiento}http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=7ac3592da4a3506b0eda70da655c0ae1`)
+            .then(function (response) {
+                // handle success
+               /*  console.log(response.data.message.body.lyrics.lyrics_body); */
+                /* letra1 = response.data.message.body.lyrics.lyrics_body;
+                console.log(letra1); */
+                // alert(response.data.lyrics);
+                /* var lyrics1 = response.data.lyrics;
+                body.lyrics = lyrics1; */
+                
+                })
+                .catch(()=> {
+                   /*  setLoading(false);
+                    setError('Error') */
+                });
+
+              
+  
+  
 
 
   return (
@@ -42,20 +65,23 @@ function App() {
           <input type="text" className="form-control"  onChange={handleChange} /> {/*handleChange se dispara el evento cada que se realice un cambio */}
         </div>
         <div className="row">
-         {/* {nomCancion.map(( cancion) => <Cancion nombre={cancion.nombre} album="Barrio Fino" cantante="Daddy Yankee" />)}  */}
-         {nomCancion.map((cancion) => <Cancion nombre={cancion.track.track_name} album={cancion.track.album_name} cantante={cancion.track.artist_name} />)}
-          <Card img="imagen" />
+         {/* {nomCancion.map(( cancion) => <Cancion nombre={cancion.nombre} album="Barrio Fino" cantante="Daddy Yankee" />) */}
+         {nomCancion.map((cancion) => <Cancion nombre={cancion.track.track_name} album={cancion.track.album_name} cantante={cancion.track.artist_name} rating={cancion.track.track_rating}/>)}
+          
+          
+          {/* <Card img="imagen" /> */}
 
 
 
-        {/* {nomCancion.map(( nomCancion) => <Card nombre={nomCancion.title}  />)} */}
-          <Card img="imagen" />
+
+          {/* {nomCancion.map(( nomCancion) => <Card nombre={nomCancion.title}  />)} */}
+          {/* <Card img="imagen" /> */}
         </div>
       </div>
-      <FiltroCancion />
-      <FiltroCancion />
-      <Cancion nombre="gasolina" album="Barrio Fino" cantante="Daddy Yankee" />
-      <Cancion nombre="Camisa negra" album="Mi Sangre" cantante="Juanes" />
+      {/* <FiltroCancion />
+      <FiltroCancion /> */}
+      {/* <Cancion nombre="gasolina" album="Barrio Fino" cantante="Daddy Yankee" />
+      <Cancion nombre="Camisa negra" album="Mi Sangre" cantante="Juanes" /> */}
     </section>
   );
 }
--- app-lyrics/src/components/Cancion/Cancion.js
+++ app-lyrics/src/components/Cancion/Cancion.js
@@ -9,11 +9,13 @@ const FiltroCancion = (props) => { // props se usa como un objeto y .para llamar
             <h4>Nombre: {props.nombre}</h4>
             <p>Album: {props.album}</p>
             <p>Cantante: {props.cantante}</p>
+            <p>Rating: {props.rating}</p>
+            <p>Letra: {props.letra} </p>
 
             <p>{song}</p>
             {/* comentar  Shift + Alt + A */}
             {/*  cada vez que le de clic en boton Buscar voy a ejecutar una funcion anonima que va a retornan la ejecucion de seSong*/}
-            <button onClick={() => setSong('GET SONG') }> Busca </button>
+            <button onClick={() => setSong('GET SONG') }> Buscar </button>
         </div>
     )
 }
